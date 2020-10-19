f367049187bb0499ddfac2210ffe6cff74f2bc4f
Sebastien Peytrignet <Sebastien.Peytrignet@health.org.uk>
2020-10-19 16:03:36 +0100

    QA by Seb of codes

---
+++ 1. Importing and cleaning data clean.R
@@ -0,0 +1,223 @@
+##########################################################
+################### DEVELOPMENT IDEAS ####################
+##########################################################
+
+##############################################
+################### SETUP ####################
+##############################################
+
+#Load packages
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(dplyr,stringr,sp,ggplot2,plyr,readODS,
+               gmodels,Rmisc,DescTools,data.table,
+               Hmisc,tibble,pbapply,pbmcapply,here,
+               tidyverse,readxl)
+
+#Clean up the global environment
+rm(list = ls())
+
+#Set directory where inputs are saved
+rawdatadir <- "M:/Analytics/Networked Data Lab/Shielding/"
+
+#Git directory
+gitdir <- dirname(rstudioapi::getSourceEditorContext()$path)
+
+##############################################
+################### GEO-DATA #################
+##############################################
+
+#England
+
+LA_to_higher_geo <- fread(paste0(rawdatadir,"Other data/Local_Authority_District_to_Region_(April_2019)_Lookup_in_England.csv"), header=TRUE, sep=",", check.names=T)
+
+##############################################
+################### POPULATION ###############
+##############################################
+
+#UK, Local Authority and above
+
+pop_by_LA <- read_excel(paste0(rawdatadir,"Other data/Mid-year population estimates/LA/ukmidyearestimates20192019ladcodes.xlsx"),
+                        sheet = "MYE2 - Persons",skip=4) %>%
+  select(.,Code,Name,`All ages`,age65plus) %>%
+  rename(.,LAD19CD=Code,LAD19NM=Name,pop19=`All ages`) %>%
+  filter(.,!is.na(LAD19NM)) %>%
+  mutate(.,pct_over65_18=age65plus/pop19*100) %>%
+  as.data.table()
+
+#Join with geo-indicators to match English local authorities to regions
+
+pop_by_LA <- dplyr::left_join(pop_by_LA,select(LA_to_higher_geo,"LAD19CD","RGN19CD","RGN19NM"),
+                              by=c("LAD19CD"="LAD19CD"))
+
+#Save
+
+fwrite(pop_by_LA, file = here::here("Clean data","pop_by_LA.csv"), sep = ",")
+
+##############################################
+################### DEPRIVATION ##############
+##############################################
+
+#LA level
+
+IMD2019_LA <- fread(paste0(rawdatadir,"Other data/IMD/Local authority/download1677836969253211150.csv"), header=TRUE, sep=",", check.names=T) %>%
+  filter(.,DateCode==2019)
+
+ranks <- select(IMD2019_LA,'Measurement','Value','Indices.of.Deprivation') %>%
+  filter(.,Measurement=="Rank of average score"&Indices.of.Deprivation=="a. Index of Multiple Deprivation (IMD)") %>%
+  select(.,'Value') %>% unique(.)
+
+#LA level - rank of average score
+
+IMD2019_LA_rank_IMD <- filter(IMD2019_LA,Measurement=='Rank of average score'&
+                                Indices.of.Deprivation=='a. Index of Multiple Deprivation (IMD)') %>%
+  rename(.,rank_imd=Value) %>% select(.,FeatureCode,rank_imd) %>%
+  mutate(.,decile_imd=cut(rank_imd, breaks=c(0,quantile(ranks$Value, probs = seq(0, 1, 1/10))[-1]), labels=1:10),
+         quintile_imd=cut(rank_imd, breaks=c(0,quantile(ranks$Value, probs = seq(0, 1, 1/5))[-1]), labels=1:5))
+
+IMD2019_LA_rank_Income <- filter(IMD2019_LA,Measurement=='Rank of average score'&
+                                   Indices.of.Deprivation=='b. Income Deprivation Domain') %>%
+  rename(.,rank_income=Value) %>% select(.,FeatureCode,rank_income) %>%
+  mutate(.,decile_income=cut(rank_income, breaks=c(0,quantile(ranks$Value, probs = seq(0, 1, 1/10))[-1]), labels=1:10),
+         quintile_income=cut(rank_income, breaks=c(0,quantile(ranks$Value, probs = seq(0, 1, 1/5))[-1]), labels=1:5))
+
+#Wide format
+
+IMD2019_LA_wide <- left_join(IMD2019_LA_rank_IMD,IMD2019_LA_rank_Income,by="FeatureCode")
+
+########################################################################
+################### NUMBER OF SHIELDERS BY LA: Wales ################### 
+########################################################################
+
+SPL_by_LA_Wales_All <- read_ods(paste0(rawdatadir,"SPL/Wales/shielded-patient-list-in-wales-during-the-coronavirus-covid-19-pandemic-as-at-27-july-2020-182.ods"), skip=4,sheet="Table_1",col_names=TRUE)
+
+SPL_by_LA_Wales_All <- dplyr::filter(SPL_by_LA_Wales_All,is.na(SPL_by_LA_Wales_All$'Local Authority Code')==FALSE) %>%
+  rename(.,LA.Code='Local Authority Code',Patient.Count='All Ages',LA.Name='Local Authority Name') %>%
+  select(.,LA.Code,Patient.Count,LA.Name)
+
+############ All shielders: Wales
+
+SPL_by_LA_Wales_All$Patient.Count <- as.numeric(SPL_by_LA_Wales_All$Patient.Count)
+
+SPL_by_LA_Wales_All$Breakdown.Field <- "ALL"
+
+############ Save
+
+fwrite(SPL_by_LA_Wales_All, file = here::here("Clean data","SPL_by_LA_Wales_All.csv"), sep = ",")
+
+####################################################################################
+################### NUMBER OF SHIELDERS BY LA: England and Wales ################### 
+####################################################################################
+
+SPL_by_LA <- fread(paste0(rawdatadir,"SPL/England/September/Coronavirus Shielded Patient List, England - Open Data with CMO DG - LA - 2020-09-16.csv"), header=TRUE, sep=",", check.names=T)
+
+############ Merge in population numbers (you will lose some Local Authorities - the way the SPL is structured is non-standard)
+
+#Manually fix code for England
+
+SPL_by_LA$LA.Code[which(SPL_by_LA$LA.Code=="ENG")] <- "E92000001"
+
+#Merge in population
+
+nrow(SPL_by_LA)
+SPL_by_LA <- left_join(SPL_by_LA,pop_by_LA,by=c("LA.Code"="LAD19CD"))
+nrow(SPL_by_LA)
+
+#Manually add in population data for rows that add together 2 local authorities
+#These are 'Hackney and City of London' and 'Cornwall and Isles of Scilly'
+
+hackney_city_london <- filter(pop_by_LA,LAD19NM=="Hackney")$pop19+filter(pop_by_LA,LAD19NM=="City of London")$pop19
+cornwall_scilly <- filter(pop_by_LA,LAD19NM=="Cornwall")$pop19+filter(pop_by_LA,LAD19NM=="Isles of Scilly")$pop19
+
+SPL_by_LA[SPL_by_LA$LA.Name %in% c("Hackney and City of London"),]$pop19 <- hackney_city_london
+SPL_by_LA[SPL_by_LA$LA.Name %in% c("Hackney and City of London"),]$RGN19NM <- "London"
+
+SPL_by_LA[SPL_by_LA$LA.Name %in% c("Cornwall and Isles of Scilly"),]$pop19 <- cornwall_scilly
+SPL_by_LA[SPL_by_LA$LA.Name %in% c("Cornwall and Isles of Scilly"),]$RGN19NM <- "South West"
+
+############ Save
+
+fwrite(SPL_by_LA, file = here::here("Clean data","SPL_by_LA.csv"), sep = ",")
+
+############ Reduced dataset for all shielders
+
+SPL_by_LA_All <- dplyr::filter(SPL_by_LA,Breakdown.Field=="ALL")
+
+#You lose the following LAs: no exact match with latest population figures
+# Buckinghamshire
+#NHS England does not report numbers of shielding patients for ALL local authorities
+
+SPL_by_LA_All$Patient.Count <- as.numeric(SPL_by_LA_All$Patient.Count)
+
+SPL_by_LA_All <- dplyr::mutate(SPL_by_LA_All,Shielders_pct=Patient.Count/pop19*100) %>%
+  arrange(.,desc(Shielders_pct)) %>% as.data.table()
+
+############ Merge in deprivation
+
+nrow(SPL_by_LA_All)
+SPL_by_LA_All <- left_join(SPL_by_LA_All,IMD2019_LA_wide,by=c("LA.Code"="FeatureCode"))
+nrow(SPL_by_LA_All)
+
+############ Remove rows that don't have a region or population (ENGLAND and Buckinghamshire)
+
+SPL_by_LA_All_incl_ENG <- SPL_by_LA_All
+
+SPL_by_LA_All <- dplyr::filter(SPL_by_LA_All, !is.na(RGN19NM))
+
+############ Save
+
+fwrite(SPL_by_LA_All, file = here::here("Clean data","SPL_by_LA_All.csv"), sep = ",")
+
+############ Number of shielders by disease group
+
+#Subset of data
+SPL_by_LA_dgroup <- filter(SPL_by_LA,Breakdown.Field=="Disease Group") %>%
+  rename(.,Cases.Count=Patient.Count)
+
+#Clean up number variable
+SPL_by_LA_dgroup$Cases.Count[which(SPL_by_LA_dgroup$Cases.Count=="*")] <- NA
+SPL_by_LA_dgroup$Cases.Count <- as.numeric(SPL_by_LA_dgroup$Cases.Count)
+
+#Simplify groups into 7 broad groups
+SPL_by_LA_dgroup$group <- mapvalues(SPL_by_LA_dgroup$Breakdown.Value,
+                                    from = c("Transplants (Solid)","Transplants (Haematological within 6 months)",
+                                             "Transplants (Haematological with Immunosuppression)",
+                                             "Cancer (Solid with Chemotherapy)","Cancer (Lung with Radical Radiotherapy)",
+                                             "Cancer (Haematological within 24 months)","Respiratory (Severe Asthma)",
+                                             "Respiratory (Severe COPD)","Respiratory (Severe Permanent)",
+                                             "Corticosteroid Safety Card","Rare genetic metabolic and autoimmune diseases",
+                                             "Immunosuppression Therapy","Pregnant with Congenital Heart Defect"),
+                                    to = c("Transplants","Transplants","Transplants",
+                                           "Cancer","Cancer","Cancer",
+                                           "Respiratory","Respiratory","Respiratory",
+                                           "Steroid","Rare genetic metabolic and autoimmune",
+                                           "Immunosuppression Therapy",
+                                           "Pregnant with Congenital Heart Defect"))
+
+SPL_by_LA_dgroup <- as.data.table(SPL_by_LA_dgroup)
+
+SPL_by_LA_dgroup <- SPL_by_LA_dgroup[, list(Cases.Count=sum(Cases.Count,na.rm=TRUE),
+                                            LA.Name=first(LA.Name),
+                                            pop19=first(pop19)),
+                                     by = list(LA.Code,group)]
+
+#Merge-in the Total counts
+SPL_by_LA_dgroup <- left_join(SPL_by_LA_dgroup,select(SPL_by_LA_All_incl_ENG,LA.Code,Patient.Count),
+                              by="LA.Code")
+
+#Percentage of shielded patients shielding for a given reason
+SPL_by_LA_dgroup <- mutate(SPL_by_LA_dgroup,Patient.Pct=Cases.Count/Patient.Count*100)
+
+#Save
+
+fwrite(SPL_by_LA_dgroup, file = here::here("Clean data","SPL_by_LA_dgroup.csv"), sep = ",")
+
+############ Number of shielders by disease group and deprivation
+
+SPL_by_LA_dgroup_dep <- left_join(SPL_by_LA_dgroup,select(IMD2019_LA_wide,FeatureCode,decile_income,
+                                                          quintile_income,decile_imd,quintile_imd),
+                                  by=c("LA.Code"="FeatureCode")) %>%
+  filter(.,!is.na(decile_income)) %>%
+  select(.,LA.Code,group,Cases.Count,LA.Name,pop19,Patient.Count,
+         decile_income,quintile_income,decile_imd,quintile_imd)
+
+fwrite(SPL_by_LA_dgroup_dep, file = here::here("Clean data","SPL_by_LA_dgroup_dep.csv"), sep = ",")
--- 2. Graphs clean.R
+++ 2. Graphs clean.R
@@ -14,21 +14,8 @@ pacman::p_load(dplyr,stringr,sp,ggplot2,plyr,readODS,
                pbapply,pbmcapply,here,rgdal,RColorBrewer,ggthemes,
                ggchicklet,tidyverse,showtext,ggchicklet)
 
-#install.packages("ggchicklet", repos = "https://cinc.rud.is")
-
-#Load fonts
-
-#font_add_google("Roboto Mono", "roboto-mono")
-#font_add_google("Roboto", "roboto")
-font_add_google("Quicksand", "quicksand")
-showtext_auto()
-
 #Clean up the global environment
 rm(list = ls())
-
-#Projection codes
-ukgrid = "+init=epsg:27700"
-latlong="+init=epsg:4326"
 
 #Set directory where inputs are saved
 rawdatadir <- "M:/Analytics/Networked Data Lab/Shielding/"
@@ -39,9 +26,9 @@ gitdir <- dirname(rstudioapi::getSourceEditorContext()$path)
 #One Drive directory for graphs
 onedrivegraphs <- "C:/Users/SebastienP/OneDrive - The Health Foundation/Shielding/Graphs/"
 
-############################################
-################### Load data ##############
-############################################
+##################################################
+################### Load clean data ##############
+##################################################
 
 SPL_by_LA_dgroup_dep <- fread(here::here("Clean data","SPL_by_LA_dgroup_dep.csv"), header=TRUE, sep=",", check.names=T)
 
@@ -51,13 +38,9 @@ SPL_by_LA <- fread(here::here("Clean data","SPL_by_LA.csv"), header=TRUE, sep=",
 
 SPL_by_LA_All <- fread(here::here("Clean data","SPL_by_LA_All.csv"), header=TRUE, sep=",", check.names=T)
 
-############################################################
-################### Best and worst performing ##############
-############################################################
+SPL_by_LA_Wales_All <- fread(here::here("Clean data","SPL_by_LA_Wales_All.csv"), header=TRUE, sep=",", check.names=T)
 
-SPL_by_LA_All <- arrange(SPL_by_LA_All, desc(Shielders_pct))
-head(SPL_by_LA_All,n=10)
-tail(SPL_by_LA_All,n=10)
+pop_by_LA <- fread(here::here("Clean data","pop_by_LA.csv"), header=TRUE, sep=",", check.names=T)
 
 ###########################################################
 ############## Reasons for shielding dot plot #############
@@ -66,11 +49,11 @@ tail(SPL_by_LA_All,n=10)
 dotplot_condition <- ggplot(filter(SPL_by_LA_dgroup,LA.Name=="ENGLAND"),
                             aes(Patient.Pct, reorder(group, Patient.Pct))) +
   geom_point(size=4,col="springgreen4") +
-  geom_text(aes(x=Patient.Pct+3,label=paste0(round(Patient.Pct,1),"%")),
-            family="quicksand",size=25) +
+  geom_text(aes(x=Patient.Pct+2,label=paste0(round(Patient.Pct,1),"%")),
+            size=3) +
   theme(panel.background = element_blank(),axis.ticks.x=element_blank(),
         axis.text.x=element_blank(),axis.ticks.y=element_blank(),
-        text = element_text(family = "quicksand", size = 80)) +
+        text = element_text(size = 10)) +
   labs(title="Conditions",y = " ",x=" ")
 
 windows()
@@ -94,20 +77,18 @@ pct_shielding_by_income_dep <- SPL_by_LA_All[, list(
 cols <- colorRampPalette(brewer.pal(n = 9, name = "RdYlGn"))(10)
 deplabs <- c("1 (most deprived)",2:9,"10 (least deprived)")
 
-as_tibble(pct_shielding_by_income_dep)
-
 by_deprivation <- ggplot(pct_shielding_by_income_dep) +
   geom_chicklet(aes(x=factor(decile_income), y = Shielders_pct,fill=factor(decile_income)),
                 radius = grid::unit(5, 'mm'),width = 0.75) +
   geom_text(aes(x = decile_income, y = Shielders_pct + 0.2,
                 label = paste0(round(Shielders_pct, 1),"%")),
-            family="quicksand",size=25) +
+            size=5) +
   theme(panel.border = element_blank(),
         panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_blank(),axis.text.y=element_blank(),
         axis.ticks.y=element_blank(),axis.ticks.x=element_blank(),
-        text = element_text(family = "quicksand", size = 80),
-        axis.text = element_text(size = 80),
+        text = element_text(size = 10),
+        axis.text = element_text(size = 10),
         axis.title.x = element_text(margin = unit(c(3, 0, 0, 0), "mm")))  +
   scale_fill_manual(values=cols,guide=FALSE) +
   scale_x_discrete(labels = deplabs) +
@@ -124,15 +105,12 @@ ggsave(paste0(gitdir,"/Charts/","by_deprivation_dec.png"), by_deprivation, devic
 ################### Deprivation vs. reason for shielding ##############
 #######################################################################
 
-filter(SPL_by_LA_dgroup_dep,LA.Name=="Liverpool")
-
 SPL_by_LA_dgroup_dep <- as.data.table(SPL_by_LA_dgroup_dep)
 
 SPL_by_LA_dgroup_dep_sum <- SPL_by_LA_dgroup_dep[, list(Cases.Count=sum(Cases.Count,na.rm=TRUE),
                                                         Patient.Count=sum(Patient.Count,na.rm=TRUE)),
                                      by = list(decile_income,group)]
 
-#SPL_by_LA_dgroup_dep_sum <- within(SPL_by_LA_dgroup_dep_sum, {Cases.Total = ave(Cases.Count,decile_income,FUN=sum)} )
 SPL_by_LA_dgroup_dep_sum <- mutate(SPL_by_LA_dgroup_dep_sum,Cases.Pct=Cases.Count/Patient.Count*100)
 
 deplabs <- c("1 (most deprived)",2:9,"10 (least deprived)")
@@ -140,7 +118,7 @@ deplabs <- c("1 (most deprived)",2:9,"10 (least deprived)")
 plot_reason_dep <- ggplot(SPL_by_LA_dgroup_dep_sum, aes(fill=factor(decile_income), y=Cases.Pct, x=group)) + 
   geom_bar(position="dodge", stat="identity") +
   geom_text(aes(label=round(Cases.Pct,0)), position=position_dodge(width=0.9),
-            vjust=-0.25, family="quicksand",size=20) +
+            vjust=-0.25,size=3) +
   scale_x_discrete(guide = guide_axis(n.dodge=3))+
   scale_fill_brewer(palette="Spectral",name = "IMD income decile (2019)",
                     labels=deplabs) +
@@ -149,8 +127,8 @@ plot_reason_dep <- ggplot(SPL_by_LA_dgroup_dep_sum, aes(fill=factor(decile_incom
         panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
         panel.background = element_blank(),axis.text.y=element_blank(),
         axis.ticks.y=element_blank(),axis.ticks.x=element_blank(),
-        text = element_text(family = "quicksand", size = 80),
-        axis.text = element_text(size = 80),
+        text = element_text(size = 20),
+        axis.text = element_text(size = 20),
         axis.title.x = element_text(margin = unit(c(3, 0, 0, 0), "mm"))) 
 
 windows()
@@ -172,17 +150,26 @@ SPL_by_GOR <- SPL_by_LA_All[, list(Shielders_pct=weighted.mean(Shielders_pct,pop
 
 #Add other countries
 
-filter(SPL_by_LA_All,RGN19NM=="London")
-filter(SPL_by_LA_All,RGN19NM=="South West")
-filter(SPL_by_LA_All,RGN19NM=="Wales")
+#Number of shielders
+nr_shielders_Wales <- filter(SPL_by_LA_Wales_All,LA.Name=="Wales")$Patient.Count
+nr_shielders_Scotland <- 179997
+  #Source: https://www.publichealthscotland.scot/media/2763/covid-19-shielding-programme-scotland-impact-and-experience-survey_sept2020_english.pdf
+nr_shielders_NI <- 80000
+  #Source: https://patientclientcouncil.hscni.net/covid-19-and-shielding/
 
-Wales <- as.data.frame(t(c(132660/3152879*100,"Wales")))
+#Population numbers
+pop_Wales <- filter(pop_by_LA,LAD19NM=="WALES")$pop19
+pop_Scotland <- filter(pop_by_LA,LAD19NM=="SCOTLAND")$pop19
+pop_NI <- filter(pop_by_LA,LAD19NM=="NORTHERN IRELAND")$pop19
+
+#Create dataframes
+Wales <- as.data.frame(t(c(nr_shielders_Wales/pop_Wales*100,"Wales")))
 names(Wales) <- names(SPL_by_GOR)[2:1]
 
-Scotland <- as.data.frame(t(c(179997/5463300*100,"Scotland")))
+Scotland <- as.data.frame(t(c(nr_shielders_Scotland/pop_Scotland*100,"Scotland")))
 names(Scotland) <- names(SPL_by_GOR)[2:1]
 
-NI <- as.data.frame(t(c(80000/(1893700)*100,"Northern Ireland")))
+NI <- as.data.frame(t(c(nr_shielders_NI/pop_NI*100,"Northern Ireland")))
 names(NI) <- names(SPL_by_GOR)[2:1]
 
 SPL_by_GOR <- rbind(SPL_by_GOR,Scotland,NI,Wales)
@@ -194,9 +181,9 @@ SPL_by_GOR$Shielders_pct <- as.numeric(SPL_by_GOR$Shielders_pct)
 plot_by_gor <- ggplot(SPL_by_GOR) +
   geom_chicklet(aes(x=reorder(RGN19NM, Shielders_pct), y = Shielders_pct),
                 fill = "firebrick",radius = grid::unit(5, 'mm'),width=0.50) +
-  geom_text(aes(x = RGN19NM, y = Shielders_pct + 0.45,
+  geom_text(aes(x = RGN19NM, y = Shielders_pct + 0.3,
                 label = paste0(round(Shielders_pct, 1),"%")),
-            family="quicksand",size=25) +
+            size=5) +
   theme(axis.title.x = element_blank(),
         panel.border = element_blank(),
         panel.grid.major = element_blank(),
@@ -204,7 +191,7 @@ plot_by_gor <- ggplot(SPL_by_GOR) +
         axis.text.x=element_text(angle=45, hjust=1),axis.text.y=element_blank(),
         axis.ticks.y=element_blank(),axis.ticks.x=element_blank(),
         panel.background = element_blank(),
-        text = element_text(family = "quicksand", size = 90)) +
+        text = element_text(size = 15)) +
   labs(title="Average % residents shielding",y = " ",x="")
 
 
@@ -213,4 +200,4 @@ plot_by_gor
 
 #Save chart
 
-ggsave(paste0(gitdir,"/Charts/","by_gor.png"), plot_by_gor, device="png",width = 15, height = 4,dpi=500)
+ggsave(paste0(gitdir,"/Charts/","by_gor.png"), plot_by_gor, device="png",width = 15, height = 7,dpi=500)
